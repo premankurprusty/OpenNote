9468b60d9a9dfb91e9b4fcb3fff8a67e4b0377fd
Premankur <premankur.prusty@gmail.com>
2026-08-17 08:32:13 +0530

    added error handling

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,25 @@ mod lexer;
 mod parser;
 mod renderer;
 use std::env;
+
+use crate::error::LexerError;
 fn main() {
     let filename = env::args_os().nth(1).unwrap();
-    let tokens = lexer::lex(filename.to_str().unwrap()).unwrap();
-    let tokens = parser::parse(tokens);
-    let output = renderer::render(tokens);
+    let tokens = lexer::lex(filename.to_str().unwrap());
+    let valid_tokens;
+    match tokens {
+        Ok(value) => valid_tokens = value,
+        Err(lexer_error) => match lexer_error {
+            LexerError::FileNotFound { path } => {
+                panic!("file: {} was not found", path);
+            }
+            LexerError::InvalidExtension { error } => {
+                panic!("{}", error);
+            }
+        },
+    }
+    let document = parser::parse(valid_tokens);
+    let output = renderer::render(document);
     println!("{}", output);
     _ = std::fs::write("output.html", output);
 }
